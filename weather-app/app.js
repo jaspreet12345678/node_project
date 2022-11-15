8548c80d1d5667fa18e8ee2e21f20d2e892b30be
jaspreet12345678 <jaspreet.mangoit@gmail.com>
2022-11-15 17:45:55 +0530

    make it better

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,8 @@
-// const { response } = require("express")
-// const request  = require("request")
+const { response } = require("express")
+const request  = require("request")
 
-// const url ="http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596"
+const url ="http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596"
 
-// request({url:url , json: true},(error, response) =>{
-//     console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out. It feels like " + response.body.current.feelslike + " degrees out." )
-// })
-
-var QRCode = require('qrcode')
-const express = require("express")
-const app =express()
-
-app.get("/", (req, res) => {
-    QRCode.toDataURL('I am a pony!', function (err, url) {
-    res.send(`<img src="${url}"/>`)
-    })
+request({url:url , json: true},(error, response) =>{
+    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out. It feels like " + response.body.current.feelslike + " degrees out." )
 })
-
-app.listen(8000)
